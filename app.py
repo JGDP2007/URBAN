e46b65727c4f398c2c2a66cae3dda840324b20ae
JGDP2007 <jgduplessis25@gmail.com>
2026-05-04 13:14:12 +0200

    fixed database config properly

--- app.py
+++ app.py
@@ -12,7 +12,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret123'
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') , 'sqlite:///db.sqlite'
+uri = os.environ.get('DATABASE_URL')
+
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql+psycopg2://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri or 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SCHEDULER_TIMEZONE'] = 'Africa/Johannesburg'
 
